5261568185c574ddf5aa122348ae77b8cd70dc6d
javi <javii.oteero@gmail.com>
2024-04-09 13:28:16 +0200

    Modificación con test parametrizado

--- src/es/iesmz/tests/VocalesTest.java
+++ src/es/iesmz/tests/VocalesTest.java
@@ -1,42 +1,24 @@
 package es.iesmz.tests;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VocalesTest {
-    @Test
-    void testContarVocalesCon4() {
-        assertEquals(4, Vocales.contarVocales("Hola Mundo"));
-    }
 
-    @Test
-    void testContarVocalesCon8() {
-        assertEquals(8, Vocales.contarVocales("Esto Es una cadenA"));
-    }
-
-    @Test
-    void testContarVocalesCon7() {
-        assertEquals(7, Vocales.contarVocales("Java mola MogollOn"));
-    }
-
-    @Test
-    void testContarVocalesCon1() {
-        assertEquals(1, Vocales.contarVocales("4h0r4 s0n num3r0s"));
-    }
-
-    @Test
-    void testContarVocalesCon6() {
-        assertEquals(6, Vocales.contarVocales("Cadena S1n Vocales"));
-    }
-
-    @Test
-    void testContarVocalesConCadenaVacia() {
-        assertEquals(0, Vocales.contarVocales(""));
-    }
-
-    @Test
-    void testContarVocalesCon0() {
-        assertEquals(0, Vocales.contarVocales("cmd"));
+    @ParameterizedTest
+    @CsvSource(
+            {"'Hola Mundo', 4",
+            "'Esto Es una cadenA', 8",
+            "'Java mola MogollOn', 7",
+            "'4h0r4 s0n num3r0s', 1",
+            "'Cadena S1n Vocales', 6",
+            "'', 0",
+            "'cmd', 0"}
+    )
+    void testParametrizado(String texto, int vocales){
+        assertEquals(vocales, Vocales.contarVocales(texto));
     }
 }
